87a0735223818cf87b6e97b3dfc36ff3e22201f3
ferhatchem <ferhatchem@gmail.com>
2020-07-14 23:42:16 -0500

    First git commit with java project

---
+++ src/com/demogit/Main.java
@@ -0,0 +1,9 @@
+package com.demogit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is my first project with intellij");
+	// write your code here
+    }
+}
