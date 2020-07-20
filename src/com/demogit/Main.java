ede5a5eae21bef55ecff0c32bd8bb80c806d0e7c
ferhatchem <ferhatchem@gmail.com>
2020-07-20 15:22:38 -0500

    First git commit with java project second git commit with java project

--- src/com/demogit/Main.java
+++ src/com/demogit/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("this is my first project with intellij");
+        System.out.println("this is my second trial for loval repository");
+
+
 	// write your code here
     }
 }
